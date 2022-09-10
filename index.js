c044cdce465f9211f88005413ecd50ef4a6eed69
Naveen G <gnaveenkumar18.ng@gmail.com>
2022-09-10 15:31:24 +0530

    heroku server port changed to 5000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use('/test',(req,res)=>{
     console.log("Connected to Heroku");
     res.json({status:true,message:"Heroku Connected Successfully"});
 });
-const server = app.listen(3001, () => {
+const server = app.listen(5000, () => {
     const port = server.address().port;
     console.log(`Express is working on port ${port}`);
   });
