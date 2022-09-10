60517875c23c6440dd7815b36070d380d9f09d36
Naveen G <gnaveenkumar18.ng@gmail.com>
2022-09-10 15:03:16 +0530

    test rount created

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ let express = require('express');
 let app = express();
 const path = require('path')
 app.use('/', express.static(path.join(__dirname, '/ChromeExtension2')));
-
+app.use('/test',(req,res)=>{
+    console.log("Connected to Heroku");
+    res.json({status:true,message:"Heroku Connected Successfully"});
+})
 app.listen(3000);
