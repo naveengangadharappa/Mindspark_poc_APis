5834997a981b6c9ef889da29122e193a916b9e1f
Naveen G <gnaveenkumar18.ng@gmail.com>
2022-09-10 15:17:32 +0530

    heroku server error issue resolved

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let express = require('express'); 
 let app = express();
-const path = require('path')
+const path = require('path');
+
 app.use('/', express.static(path.join(__dirname, '/ChromeExtension2')));
 app.use('/test',(req,res)=>{
     console.log("Connected to Heroku");
     res.json({status:true,message:"Heroku Connected Successfully"});
-})
-app.listen(3000);
+});
+const server = app.listen(3000, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
+  });
+//app.listen(3000);
